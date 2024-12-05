0061fbc93095aecaa8bf273d22caa77477e9bfce
sudeep <sudeep.kunhikannan@ons.gov.uk>
2024-12-05 10:57:26 +0000

    cleanup

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,10 +1,8 @@
-import pytest
-
-from src.calculator import Calculator
+# import pytest
 
 
-@pytest.fixture()
-def calculator():
-    # Create a new instance of the Calculator class for each test session.
-    yield Calculator()
-    # Clean up after the test session is complete.
+# @pytest.fixture()
+# def calculator():
+#     # Create a new instance of the Calculator class for each test session.
+#     yield Calculator()
+#     # Clean up after the test session is complete.
