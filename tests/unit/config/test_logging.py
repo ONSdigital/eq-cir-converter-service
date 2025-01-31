6af672af70a7648b2f727e7d738d3ccabe9c13ca
sudeep <sudeep.kunhikannan@ons.gov.uk>
2025-01-31 10:50:03 +0000

    unit tests for logging

---
+++ tests/unit/config/test_logging.py
@@ -0,0 +1,19 @@
+"""Unit tests for logging configuration."""
+
+import logging
+import os
+
+from eq_cir_converter_service.app.config import logging_config
+
+
+def test_get_log_level():
+    """Test that the logging level is set to INFO by default."""
+    log_level = logging_config.get_log_level()
+    assert log_level == logging.INFO
+
+
+def test_get_log_level_custom():
+    """Test that the logging level is set to DEBUG when the LOG_LEVEL environment variable is set to DEBUG."""
+    os.environ["LOG_LEVEL"] = "DEBUG"
+    log_level = logging_config.get_log_level()
+    assert log_level == logging.DEBUG
