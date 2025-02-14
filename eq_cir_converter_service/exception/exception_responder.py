fed9439213127fca04eaf8d2c76649be7f4f6bd6
sudeep <sudeep.kunhikannan@ons.gov.uk>
2025-02-14 09:40:08 +0000

    Add ExceptionResponder class

---
+++ eq_cir_converter_service/exception/exception_responder.py
@@ -0,0 +1,24 @@
+"""This module contains the exception responder for the FastAPI application."""
+
+from fastapi.responses import JSONResponse
+
+from eq_cir_converter_service.exception.exception_response_models import ExceptionResponseModel
+
+
+class ExceptionResponder:
+    """The class to respond to exceptions with a JSON response."""
+
+    status_code: int
+    response: ExceptionResponseModel
+
+    def __init__(self, status_code: int, response: ExceptionResponseModel) -> None:
+        """Initialise the ExceptionResponder."""
+        self.status_code = status_code
+        self.response = response
+
+    def throw_er_with_json(self) -> JSONResponse:
+        """Throw the exception with a JSON response."""
+        return JSONResponse(
+            status_code=self.status_code,
+            content=self.response.__dict__,
+        )
