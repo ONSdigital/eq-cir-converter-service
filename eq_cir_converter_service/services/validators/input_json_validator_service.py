075791b391b9579b8bccd2267fccd45c46f2eb68
sudeep <sudeep.kunhikannan@ons.gov.uk>
2025-02-14 09:38:15 +0000

    empty input json validation

---
+++ eq_cir_converter_service/services/validators/input_json_validator_service.py
@@ -0,0 +1,20 @@
+from eq_cir_converter_service.config.logging_config import logging
+from eq_cir_converter_service.exception import exceptions
+
+logger = logging.getLogger(__name__)
+
+class InputJSONValidatorService:
+    """Service class for validating the input JSON."""
+
+    @staticmethod
+    def validate_input_json(schema: dict) -> None:
+        """Validates the input JSON schema to ensure it is not empty.
+
+        Parameters:
+        - schema: The input JSON schema.
+        """
+        if not schema:
+            logger.error("Input JSON schema is empty")
+            raise exceptions.InputJSONValidationException
+        logger.info("Input JSON schema is not empty")
+        return None
