c0d22d7686bd3d45d68650cf268c91b8aac4cfc0
sudeep <sudeep.kunhikannan@ons.gov.uk>
2025-02-19 11:26:11 +0000

    tests using env values

--- tests/unit/routers/test_schema_router.py
+++ tests/unit/routers/test_schema_router.py
@@ -2,12 +2,18 @@
 
 from fastapi import status
 from fastapi.testclient import TestClient
+import eq_cir_converter_service.exception.exception_response_models as erm
+
+from eq_cir_converter_service.config.config_helpers import get_current_version_env, get_target_version_env
+
+env_current_version = get_current_version_env()
+env_target_version = get_target_version_env()
 
 
 def test_schema_router_with_valid_json(test_client: TestClient) -> None:
     """Test the post schema method with valid JSON."""
     response = test_client.post(
-        "/schema?current_version=9.0.0&target_version=10.0.0",
+        f"/schema?current_version={env_current_version}&target_version={env_target_version}",
         json={"valid_json": "valid_json"},
     )
     assert response.status_code == status.HTTP_200_OK
@@ -21,54 +27,54 @@ def test_schema_router_with_invalid_current_version(test_client: TestClient) -> 
         json={"valid_json": "valid_json"},
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {"status": "error", "message": "Invalid current version received"}
+    assert response.json() == {"status": "error", "message": erm.erm_400_invalid_current_version_exception.message}
 
 
 def test_schema_router_with_invalid_target_version(test_client: TestClient) -> None:
     """Test the post schema method with invalid target version."""
     response = test_client.post(
-        "/schema?current_version=9.0.0&target_version=2",
+        f"/schema?current_version={env_current_version}&target_version=2",
         json={"valid_json": "valid_json"},
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {"status": "error", "message": "Invalid target version received"}
+    assert response.json() == {"status": "error", "message": erm.erm_400_invalid_target_version_exception.message}
 
 
 def test_schema_router_with_current_version_pattern_match(test_client: TestClient) -> None:
     """Test the post schema method with invalid target version."""
     response = test_client.post(
-        "/schema?current_version=1.0.0&target_version=2",
+        f"/schema?current_version=1.0.0&target_version={env_target_version}",
         json={"valid_json": "valid_json"},
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {"status": "error", "message": "Invalid current version received"}
+    assert response.json() == {"status": "error", "message": erm.erm_400_invalid_current_version_exception.message}
 
 
 def test_schema_router_with_target_version_pattern_match(test_client: TestClient) -> None:
     """Test the post schema method with invalid target version."""
     response = test_client.post(
-        "/schema?current_version=9.0.0&target_version=2.0.0",
+        f"/schema?current_version={env_current_version}&target_version=2.0.0",
         json={"valid_json": "valid_json"},
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {"status": "error", "message": "Invalid target version received"}
+    assert response.json() == {"status": "error", "message": erm.erm_400_invalid_target_version_exception.message}
 
 
 def test_schema_router_with_empty_json(test_client: TestClient) -> None:
     """Test the post schema method with empty JSON."""
     response = test_client.post(
-        "/schema?current_version=9.0.0&target_version=10.0.0",
+        f"/schema?current_version={env_current_version}&target_version={env_target_version}",
         json={},
     )
     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {"status": "error", "message": "Input JSON schema is empty"}
+    assert response.json() == {"status": "error", "message": erm.erm_400_empty_input_json_exception.message}
 
 
 def test_schema_router_to_simulate_processing_exception(test_client: TestClient) -> None:
     """Test the post schema method with empty JSON."""
     response = test_client.post(
-        "/schema?current_version=9.0.0&target_version=10.0.0",
+        f"/schema?current_version={env_current_version}&target_version={env_target_version}",
         json={"simulate_processing_exception": "simulate_processing_exception"},
     )
     assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
-    assert response.json() == {"status": "error", "message": "Error encountered while processing the schema"}
+    assert response.json() == {"status": "error", "message": erm.erm_500_schema_processing_exception.message}
