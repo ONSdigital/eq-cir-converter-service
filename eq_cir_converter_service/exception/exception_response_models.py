f4a6e587ab734d58d80db88a0f9bb411d2169e4c
sudeep <sudeep.kunhikannan@ons.gov.uk>
2025-02-19 11:25:45 +0000

    exception messages using env values

--- eq_cir_converter_service/exception/exception_response_models.py
+++ eq_cir_converter_service/exception/exception_response_models.py
@@ -1,6 +1,10 @@
 """This module contains the Pydantic models for the exception responses that are returned by the API."""
 
 from pydantic import BaseModel
+from eq_cir_converter_service.config.config_helpers import get_current_version_env, get_target_version_env
+
+env_current_version = get_current_version_env()
+env_target_version = get_target_version_env()
 
 
 class ExceptionResponseModel(BaseModel):
@@ -15,11 +19,11 @@ erm_500_schema_processing_exception = ExceptionResponseModel(
 )
 
 erm_400_invalid_current_version_exception = ExceptionResponseModel(
-    status="error", message="Invalid current version received"
+    status="error", message=f"Invalid current version received, expected version is {env_current_version}"
 )
 
 erm_400_invalid_target_version_exception = ExceptionResponseModel(
-    status="error", message="Invalid target version received"
+    status="error", message=f"Invalid target version received, expected version is {env_target_version}"
 )
 
 erm_400_empty_input_json_exception = ExceptionResponseModel(status="error", message="Input JSON schema is empty")
