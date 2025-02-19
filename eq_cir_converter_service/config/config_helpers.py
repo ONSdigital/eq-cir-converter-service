86ae4717b48652bfe78bb8bb3de1cae87dce6acb
sudeep <sudeep.kunhikannan@ons.gov.uk>
2025-02-19 13:42:14 +0000

    add log level retrieval from environment variable

--- eq_cir_converter_service/config/config_helpers.py
+++ eq_cir_converter_service/config/config_helpers.py
@@ -1,8 +1,19 @@
+"""This module contains helper functions for the configuration of the application."""
+
+import logging
 import os
+
+
+def get_log_level() -> int:
+    """Get the logging level from the LOG_LEVEL environment variable, or use the default value of INFO."""
+    log_level = os.getenv("LOG_LEVEL", "INFO")
+    return int(getattr(logging, log_level, logging.INFO))
+
 
 def get_current_version_env() -> str:
     """Get the current version from the CURRENT_VERSION environment variable, or use the default value of 9.0.0."""
     return os.getenv("CURRENT_VERSION", "9.0.0")
+
 
 def get_target_version_env() -> str:
     """Get the target version from the TARGET_VERSION environment variable, or use the default value of 10.0.0."""
