96d3cccce8dbe97640b8ff53ae96713289ab7783
sudeep <sudeep.kunhikannan@ons.gov.uk>
2025-03-12 09:47:31 +0000

    removed schema processor service class

--- eq_cir_converter_service/services/schema/schema_processor_service.py
+++ eq_cir_converter_service/services/schema/schema_processor_service.py
@@ -5,23 +5,19 @@ from eq_cir_converter_service.config.logging_config import logging
 logger = logging.getLogger(__name__)
 
 
-class SchemaProcessorService:
-    """Service class for processing the schema."""
+def convert_schema(current_version: str, target_version: str, schema: dict) -> dict:
+    """Converts the schema from the current to the target version.
 
-    @staticmethod
-    def convert_schema(current_version: str, target_version: str, schema: dict) -> dict:
-        """Converts the schema from the current to the target version.
+    Parameters:
+    - current_version: The current version of the schema.
+    - target_version: The target version of the schema.
+    - schema: The schema to convert.
 
-        Parameters:
-        - current_version: The current version of the schema.
-        - target_version: The target version of the schema.
-        - schema: The schema to convert.
+    Returns:
+    - dict: The converted schema.
+    """
+    logger.info("Converting the schema...")
 
-        Returns:
-        - dict: The converted schema.
-        """
-        logger.info("Converting the schema...")
+    # TO DO: Implement the logic to convert the schema from one version to another
 
-        # TO DO: Implement the logic to convert the schema from one version to another
-
-        return schema
+    return schema
