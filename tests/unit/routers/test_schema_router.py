b56e2821c2fe5f81049edfaf72053f69e277e0fd
farres1 <sam.farrell@ons.gov.uk>
2025-04-23 14:39:51 +0100

    Format test schema router file

--- tests/unit/routers/test_schema_router.py
+++ tests/unit/routers/test_schema_router.py
@@ -77,7 +77,7 @@ def test_convert_schema_http_exception(test_client: TestClient) -> None:
         TARGET_VERSION,
         {"valid_json": "valid_json"},
     )
-    
+
     schema_processor_sevice.convert_schema.reset_mock()
 
 
