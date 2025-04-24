b5d8968d0852e408d75734c29027870e9e98b6c7
farres1 <sam.farrell@ons.gov.uk>
2025-04-24 10:58:03 +0100

    Replace MagicMock with Mock

--- tests/unit/routers/test_schema_router.py
+++ tests/unit/routers/test_schema_router.py
@@ -1,6 +1,6 @@
 """This module contains the unit tests for the schema router."""
 
-from unittest.mock import MagicMock
+from unittest.mock import Mock
 
 from fastapi import HTTPException, status
 from fastapi.testclient import TestClient
@@ -58,6 +58,27 @@ def test_schema_router_with_empty_json(test_client: TestClient) -> None:
     }
 
 
+def test_convert_schema_http_exception(test_client: TestClient) -> None:
+    """Test the convert schema method with an HTTPException."""
+    schema_processor_sevice.convert_schema = Mock(
+        side_effect=HTTPException(status_code=400, detail="Mocked HTTPException"),
+    )
+    response = test_client.post(
+        f"/schema?current_version={CURRENT_VERSION}&target_version={TARGET_VERSION}",
+        json={"valid_json": "valid_json"},
+    )
+    assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+    assert response.json() == {
+        "detail": {"status": "error", "message": exception_messages.EXCEPTION_500_SCHEMA_PROCESSING},
+    }
+
+    schema_processor_sevice.convert_schema.assert_called_with(
+        CURRENT_VERSION,
+        TARGET_VERSION,
+        {"valid_json": "valid_json"},
+    )
+
+
 def test_schema_router_with_matching_versions(test_client: TestClient) -> None:
     """Test the post schema method with current version and target version equal."""
     current = "10.0.0"
@@ -80,26 +101,3 @@ def test_schema_router_with_new_version(test_client: TestClient) -> None:
     )
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["valid_json"] == "valid_json"
-
-
-def test_convert_schema_http_exception(test_client: TestClient) -> None:
-    """Test the convert schema method with an HTTPException."""
-    schema_processor_sevice.convert_schema = MagicMock(
-        side_effect=HTTPException(status_code=400, detail="Mocked HTTPException"),
-    )
-    response = test_client.post(
-        f"/schema?current_version={CURRENT_VERSION}&target_version={TARGET_VERSION}",
-        json={"valid_json": "valid_json"},
-    )
-    assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
-    assert response.json() == {
-        "detail": {"status": "error", "message": exception_messages.EXCEPTION_500_SCHEMA_PROCESSING},
-    }
-
-    schema_processor_sevice.convert_schema.assert_called_with(
-        CURRENT_VERSION,
-        TARGET_VERSION,
-        {"valid_json": "valid_json"},
-    )
-
-    schema_processor_sevice.convert_schema.reset_mock()
